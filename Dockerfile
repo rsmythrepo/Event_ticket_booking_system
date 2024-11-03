c87d83793fe938457217d73729817605558dac05
rsmythrepo <raphaelle.smyth@gmail.com>
2024-11-03 23:07:46 +0000

    docker compose config

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,6 @@ FROM python:3.9-slim
 
 WORKDIR /app
 
-RUN apt-get update && apt-get install -y \
-    default-libmysqlclient-dev build-essential \
-    && apt-get clean
-
 COPY requirements.txt /app/
 RUN pip install --no-cache-dir -r requirements.txt
 
--- __init__.py
+++ __init__.py
@@ -10,7 +10,12 @@ if os.getenv("FLASK_ENV") == "testing":
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/test_event_bookings'
     app.config['TESTING'] = True
 else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/event_bookings'
+    db_user = os.getenv("DB_USER", "root")
+    db_password = os.getenv("DB_PASSWORD", "root")
+    db_host = os.getenv("DB_HOST", "localhost")  # Use "mysql-db" for Docker
+    db_name = os.getenv("DB_NAME", "event_bookings")
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:3306/{db_name}'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
